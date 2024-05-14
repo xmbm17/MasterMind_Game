aeaba101ded8d3d4ce4b66a53850752dc86c07f2
xmbm17 <markmart17@icloud.com>
2024-05-14 00:20:36 -0400

    Adding project to GitHub repo

---
+++ Mark_Martinez_MasterMind/Mastermind.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mark_Martinez_MasterMind
+{
+    public class Mastermind
+    {
+        string difficultyLevel;
+        int chances;
+        public Mastermind(string difficulty)
+        {
+            difficultyLevel = difficulty;
+            chances = 10;
+        }
+
+        public async Task StartGame()
+        {
+            string[] pastGuesses = { "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" };
+
+            string randomCombination = await RandomNumApiCall(difficultyLevel);
+
+            randomCombination = string.Join("", randomCombination.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries));
+
+            Console.WriteLine(randomCombination, "here is the random combo");
+            Console.WriteLine($"You will have 10 attempts to guess the {difficultyLevel} digit combination, goodluck!\n");
+
+            while (chances > 0)
+            {
+                Console.WriteLine($"Enter your {difficultyLevel} guess, you have {chances} chances left");
+
+                string userGuess = Console.ReadLine();
+
+                if (userGuess.Length != Convert.ToInt32(difficultyLevel))
+                {
+                    Console.WriteLine($"Please enter a {difficultyLevel} digit combination");
+                }
+                else
+                {
+
+                    if (userGuess == randomCombination)
+                    {
+                        Console.WriteLine("Congrats, you've won, you're a MasterMind!");
+                        break;
+                    }
+                    else
+                    {
+                        int numbersCorrect = 0;
+                        int numbersInCorrectPlaces = 0;
+
+                        pastGuesses[chances - 1] = userGuess;
+
+                        for (int i = 0; i < Convert.ToInt32(difficultyLevel); i++)
+                        {
+                            if (randomCombination.Contains(userGuess[i]))
+                            {
+                                numbersCorrect++;
+                            }
+                            if (randomCombination[i] == userGuess[i])
+                            {
+                                numbersInCorrectPlaces++;
+                            }
+                        }
+                        Console.WriteLine();
+
+                        if (numbersCorrect == 0)
+                        {
+                            Console.WriteLine("All incorrect.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{numbersCorrect} numbers are correct and {numbersInCorrectPlaces} are in the correct places.");
+                        }
+
+                        Console.WriteLine("Here are your past guesses: ");
+
+                        foreach (string guess in pastGuesses)
+                        {
+                            Console.WriteLine(guess);
+                        }
+
+                        chances--;
+                    }
+                }
+            }
+            if (chances == 0)
+            {
+                Console.WriteLine($"Sorry you're out of chances, the correct combination was : {randomCombination}");
+
+            }
+        }
+
+
+        static async Task<string> RandomNumApiCall(string difficulty)
+        {
+            string finalData = "";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+
+                    string baseUrl = "https://www.random.org/integers/";
+
+                    HttpResponseMessage response = await client.GetAsync($"{baseUrl}?num={difficulty}&min=0&max=7&col=4&base=10&format=plain&rnd=new");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        finalData = await response.Content.ReadAsStringAsync();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occured : " + e.Message);
+                }
+
+            };
+            return finalData;
+        }
+
+    }
+}
---
+++ Mark_Martinez_MasterMind/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Mark_Martinez_MasterMind
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepPlaying = true;
+            Console.WriteLine("Welcome to Mastermind!\n");
+            Console.WriteLine("Press 4 for easy, 5 for meduim, and 6 for hard");
+
+            string difficulty = Console.ReadLine();
+
+            while (keepPlaying)
+            {
+                Mastermind mastermind = new Mastermind(difficulty);
+                mastermind.StartGame().GetAwaiter().GetResult();
+
+                Console.WriteLine("Do you want to play again? Press y for yes and n for no.");
+
+                if (Console.ReadLine().ToLower() == "n")
+                {
+                    keepPlaying = false;
+                }
+            }
+        }
+    }
+}
